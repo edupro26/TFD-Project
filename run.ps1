584449c5f04b1bc9405f44414c0d6825b37cd877
Golaco <fc58212@alunos.fc.ul.pt>
2024-11-04 15:50:35 +0000

    fix node id script

--- run.ps1
+++ run.ps1
@@ -3,7 +3,7 @@ $scriptPath = "./src/main.py"
 $instances = 5
 $epoch_delay = 2
 
-for ($i = 1; $i -le $instances; $i++) {
+for ($i = 0; $i -le ($instances - 1); $i++) {
     Start-Process -FilePath $pythonPath -ArgumentList "$scriptPath --node-id $i --epoch-delay $epoch_delay --total-nodes $instances"
 }
 
--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ script_path="./src/main.py"
 instances=5
 epoch_delay=2
 
-for ((i=1; i<=instances; i++)); do
+for ((i=0; i<=(instances - 1); i++)); do
     python "$script_path" --node-id "$i" --epoch-delay "$epoch_delay" --total-nodes "$instances" &
 done
 
