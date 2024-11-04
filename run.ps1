5bf4073e3e7d941a9beb5c2d726cb3caf220e5f1
Golaco <fc58212@alunos.fc.ul.pt>
2024-11-04 14:32:02 +0000

    fix scripts no indice para todos os nodes correrem bem

--- run.ps1
+++ run.ps1
@@ -3,8 +3,9 @@ $scriptPath = "./src/main.py"
 $instances = 5
 $epoch_delay = 2
 
-for ($i = 1; $i -le $instances; $i++) {
+for ($i = 0; $i -le ($instances -1); $i++) {
     Start-Process -FilePath $pythonPath -ArgumentList "$scriptPath --node-id $i --epoch-delay $epoch_delay --total-nodes $instances"
+    Start-Sleep -Seconds 1
 }
 
 Write-Output "Started $instances instances."
